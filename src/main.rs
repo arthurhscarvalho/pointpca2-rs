86e7a4db2b1662e39710aa7b21e69b04cb14b589
akaTsunemori <arthur_07q@hotmail.com>
2024-06-05 18:12:12 -0300

    Add initial implementation for the textural predictors from predictors.rs

--- src/main.rs
+++ src/main.rs
@@ -45,27 +45,27 @@ fn main() {
             137, 187, 110, 137, 188, 110, 137, 187, 110, 137, 187, 110, 137,
         ],
     );
-    // let knn_indices_a = na::DMatrix::from_row_slice(
-    //     10,
-    //     9,
-    //     &[
-    //         0, 2, 1, 3, 6, 5, 4, 7, 8, 1, 2, 5, 6, 0, 3, 9, 4, 7, 2, 6, 0, 1, 3, 5, 4, 7, 9, 3, 4,
-    //         2, 6, 0, 7, 1, 8, 5, 4, 3, 7, 8, 2, 6, 0, 1, 5, 5, 1, 6, 2, 9, 3, 0, 7, 4, 6, 2, 5, 1,
-    //         3, 7, 0, 9, 4, 7, 8, 4, 3, 6, 2, 0, 5, 1, 8, 7, 4, 3, 6, 2, 0, 5, 1, 9, 5, 1, 6, 2, 3,
-    //         0, 7, 4,
-    //     ],
-    // );
-    // let knn_indices_b = na::DMatrix::from_row_slice(
-    //     10,
-    //     9,
-    //     &[
-    //         3, 5, 2, 1, 7, 4, 6, 9, 0, 5, 7, 3, 4, 2, 6, 9, 8, 1, 5, 3, 7, 2, 9, 4, 6, 8, 1, 5, 3,
-    //         7, 2, 9, 4, 6, 1, 8, 5, 7, 3, 2, 9, 4, 1, 6, 8, 7, 9, 5, 6, 4, 3, 8, 2, 1, 7, 5, 9, 3,
-    //         6, 4, 8, 2, 1, 7, 5, 9, 3, 2, 6, 4, 8, 1, 7, 5, 9, 3, 2, 6, 4, 8, 1, 9, 7, 8, 6, 5, 4,
-    //         3, 2, 1,
-    //     ],
-    // );
-    // let local_features = features::compute_features(&points_a, &colors_a, &points_b, &colors_b, &knn_indices_a, &knn_indices_b);
+    let knn_indices_a = na::DMatrix::from_row_slice(
+        10,
+        9,
+        &[
+            0, 2, 1, 3, 6, 5, 4, 7, 8, 1, 2, 5, 6, 0, 3, 9, 4, 7, 2, 6, 0, 1, 3, 5, 4, 7, 9, 3, 4,
+            2, 6, 0, 7, 1, 8, 5, 4, 3, 7, 8, 2, 6, 0, 1, 5, 5, 1, 6, 2, 9, 3, 0, 7, 4, 6, 2, 5, 1,
+            3, 7, 0, 9, 4, 7, 8, 4, 3, 6, 2, 0, 5, 1, 8, 7, 4, 3, 6, 2, 0, 5, 1, 9, 5, 1, 6, 2, 3,
+            0, 7, 4,
+        ],
+    );
+    let knn_indices_b = na::DMatrix::from_row_slice(
+        10,
+        9,
+        &[
+            3, 5, 2, 1, 7, 4, 6, 9, 0, 5, 7, 3, 4, 2, 6, 9, 8, 1, 5, 3, 7, 2, 9, 4, 6, 8, 1, 5, 3,
+            7, 2, 9, 4, 6, 1, 8, 5, 7, 3, 2, 9, 4, 1, 6, 8, 7, 9, 5, 6, 4, 3, 8, 2, 1, 7, 5, 9, 3,
+            6, 4, 8, 2, 1, 7, 5, 9, 3, 2, 6, 4, 8, 1, 7, 5, 9, 3, 2, 6, 4, 8, 1, 9, 7, 8, 6, 5, 4,
+            3, 2, 1,
+        ],
+    );
+    let local_features = features::compute_features(&points_a, &colors_a, &points_b, &colors_b, &knn_indices_a, &knn_indices_b);
     // // println!("{}", local_features);
     // for row in local_features.row_iter() {
     //     // Create a string for each row with formatted elements
@@ -76,8 +76,16 @@ fn main() {
     //     println!("{}", formatted_row.join(" "));
     // }
 
-    let knn_indices = knn_search(points_a, points_b);
-    println!("{}", knn_indices);
+    let predictors_result = predictors::compute_predictors(&local_features);
+    for row in predictors_result.row_iter() {
+        for col in row.iter() {
+            print!("{:.2} ", col);
+        }
+        println!("");
+    }
+
+    // let knn_indices = knn_search(points_a, points_b);
+    // println!("{}", knn_indices);
 
     // let num: f64 = 13.55667;
     // let num_ordered = utils::to_ordered(num);
--- src/predictors.rs
+++ src/predictors.rs
@@ -1,9 +1,116 @@
 use std::f64::EPSILON;
 
-use na::DMatrix;
+use na::{DMatrix, MatrixView};
 
 fn relative_difference(x: f64, y: f64) -> f64 {
     return 1. - (x - y).abs() / (x.abs() + y.abs() + EPSILON);
+}
+
+fn iter_relative_difference<'a, T: na::Dim>(
+    x: &'a MatrixView<f64, T, T>,
+    y: &'a MatrixView<f64, T, T>,
+) -> DMatrix<f64> {
+    assert_eq!(x.nrows(), y.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), y.ncols(), "Matrices must have the same shape.");
+    let nrows = x.nrows();
+    let ncols = x.ncols();
+    let mut result = DMatrix::zeros(nrows, ncols);
+    for i in 0..nrows {
+        for j in 0..ncols {
+            result[(i, j)] = relative_difference(x[(i, j)], y[(i, j)]);
+        }
+    }
+    result
+}
+
+fn textural_covariance<'a, T: na::Dim>(
+    x: &'a MatrixView<f64, T, T>,
+    y: &'a MatrixView<f64, T, T>,
+    z: &'a MatrixView<f64, T, T>,
+) -> DMatrix<f64> {
+    assert_eq!(x.nrows(), y.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), y.ncols(), "Matrices must have the same shape.");
+    assert_eq!(x.nrows(), z.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), z.ncols(), "Matrices must have the same shape.");
+    let nrows = x.nrows();
+    let ncols = x.ncols();
+    let mut result = DMatrix::zeros(nrows, ncols);
+    let mut x_ij: f64;
+    let mut y_ij: f64;
+    let mut z_ij: f64;
+    for i in 0..nrows {
+        for j in 0..ncols {
+            x_ij = x[(i, j)];
+            y_ij = y[(i, j)];
+            z_ij = z[(i, j)];
+            result[(i, j)] =
+                (x_ij.sqrt() * y_ij.sqrt() - z_ij).abs() / (x_ij.sqrt() * y_ij.sqrt() + EPSILON);
+        }
+    }
+    result
+}
+
+fn textural_variance_sum<'a, T: na::Dim>(
+    x: &'a MatrixView<f64, T, T>,
+    y: &'a MatrixView<f64, T, T>,
+) -> DMatrix<f64> {
+    assert_eq!(x.nrows(), y.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), y.ncols(), "Matrices must have the same shape.");
+    let nrows = x.nrows();
+    let ncols = x.ncols();
+    let mut result = DMatrix::zeros(nrows, 1);
+    for i in 0..nrows {
+        let mut x_sum: f64 = 0.;
+        let mut y_sum: f64 = 0.;
+        for j in 0..ncols {
+            x_sum += x[(i, j)];
+            y_sum += y[(i, j)];
+        }
+        result[(i, 0)] = relative_difference(x_sum, y_sum);
+    }
+    result
+}
+
+fn textural_omnivariance<'a, T: na::Dim>(
+    x: &'a MatrixView<f64, T, T>,
+    y: &'a MatrixView<f64, T, T>,
+) -> DMatrix<f64> {
+    assert_eq!(x.nrows(), y.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), y.ncols(), "Matrices must have the same shape.");
+    let nrows = x.nrows();
+    let ncols = x.ncols();
+    let mut result = DMatrix::zeros(nrows, 1);
+    for i in 0..nrows {
+        let mut x_prod: f64 = 1.;
+        let mut y_prod: f64 = 1.;
+        for j in 0..ncols {
+            x_prod *= x[(i, j)];
+            y_prod *= y[(i, j)];
+        }
+        result[(i, 0)] = relative_difference(x_prod.cbrt(), y_prod.cbrt());
+    }
+    result
+}
+
+fn textural_entropy<'a, T: na::Dim>(
+    x: &'a MatrixView<f64, T, T>,
+    y: &'a MatrixView<f64, T, T>,
+) -> DMatrix<f64> {
+    assert_eq!(x.nrows(), y.nrows(), "Matrices must have the same shape.");
+    assert_eq!(x.ncols(), y.ncols(), "Matrices must have the same shape.");
+    let nrows = x.nrows();
+    let ncols = x.ncols();
+    let mut result = DMatrix::zeros(nrows, 1);
+    for i in 0..nrows {
+        let mut x_entropy: f64 = 0.;
+        let mut y_entropy: f64 = 0.;
+        for j in 0..ncols {
+            x_entropy += x[(i, j)] * (x[(i, j)] + EPSILON).ln();
+            y_entropy += y[(i, j)] * (y[(i, j)] + EPSILON).ln();
+        }
+        result[(i, 0)] = relative_difference(x_entropy, y_entropy);
+    }
+    result
 }
 
 pub fn compute_predictors<'a>(local_features: &'a DMatrix<f64>) -> DMatrix<f64> {
@@ -27,9 +134,37 @@ pub fn compute_predictors<'a>(local_features: &'a DMatrix<f64>) -> DMatrix<f64> 
     predictors.fill(f64::NAN);
 
     // Textural predictors
-    // To-Do
-    // . . .
-
+    predictors
+        .view_mut((0, 0), (nrows, 3))
+        .copy_from(&iter_relative_difference(&colors_mean_a, &colors_mean_b));
+    predictors
+        .view_mut((0, 3), (nrows, 3))
+        .copy_from(&iter_relative_difference(
+            &colors_variance_a,
+            &colors_variance_b,
+        ));
+    predictors
+        .view_mut((0, 6), (nrows, 3))
+        .copy_from(&textural_covariance(
+            &colors_variance_a,
+            &colors_variance_b,
+            &colors_covariance_ab,
+        ));
+    predictors
+        .view_mut((0, 9), (nrows, 1))
+        .copy_from(&textural_variance_sum(
+            &colors_variance_a,
+            &colors_variance_b,
+        ));
+    predictors
+        .view_mut((0, 10), (nrows, 1))
+        .copy_from(&textural_omnivariance(
+            &colors_variance_a,
+            &colors_variance_b,
+        ));
+    predictors
+        .view_mut((0, 10), (nrows, 1))
+        .copy_from(&textural_entropy(&colors_variance_a, &colors_variance_b));
     // Geometric predictors
     // To-Do
     // . . .
