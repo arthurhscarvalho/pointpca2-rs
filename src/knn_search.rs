5a0076b8b2eacff619c4405120dd07cf44af8dea
akaTsunemori <arthur_07q@hotmail.com>
2024-06-07 21:49:56 -0300

    Bug fixes and polishing on knn_search.rs

--- src/knn_search.rs
+++ src/knn_search.rs
@@ -6,26 +6,19 @@ pub fn knn_search<'a>(
     xb: &'a DMatrix<f64>,
     search_size: usize,
 ) -> DMatrix<usize> {
-    let mut query = [0., 0., 0.];
     let mut kdtree: KdTree<f64, usize, 3, 1024, u32> = KdTree::with_capacity(xa.nrows());
     let mut knn_indices = DMatrix::zeros(xb.nrows(), search_size);
-    for i in 0..xa.nrows() {
-        query[0] = xa[(i, 0)];
-        query[1] = xa[(i, 1)];
-        query[2] = xa[(i, 2)];
-        kdtree.add(&(query.clone()), i);
-    }
-    for i in 0..xb.nrows() {
-        query[0] = xb[(i, 0)];
-        query[1] = xb[(i, 1)];
-        query[2] = xb[(i, 2)];
-        let neighbors = kdtree.nearest_n::<SquaredEuclidean>(&query, search_size);
-        let indices = neighbors
-            .into_iter()
-            .map(|nbr| nbr.item)
-            .collect::<Vec<usize>>();
+    xa.row_iter().enumerate().for_each(|(idx, point)| {
+        kdtree.add(&[point[0], point[1], point[2]], idx);
+    });
+    xb.row_iter().enumerate().for_each(|(idx, point)| {
+        let neighbors =
+            kdtree.nearest_n::<SquaredEuclidean>(&[point[0], point[1], point[2]], search_size);
+        let indices = neighbors.iter().map(|nbr| nbr.item).collect::<Vec<usize>>();
         let indices = DMatrix::from_row_slice(1, search_size, &indices);
-        knn_indices.view_mut((i, 0), (1, search_size)).copy_from(&indices);
-    }
+        knn_indices
+            .view_mut((idx, 0), (1, search_size))
+            .copy_from(&indices);
+    });
     return knn_indices;
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod utils;
 
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
-    let search_size = 81;
+    let search_size = 9;
     println!("Reading ply");
     let (points_a, colors_a) = ply_manager::read_ply_as_matrix("/home/arthurc/redandblack_vox10_1550.ply");
     let (points_b, colors_b) = ply_manager::read_ply_as_matrix("/home/arthurc/tmc13_redandblack_vox10_1550_dec_geom04_text04_octree-predlift.ply");
@@ -23,7 +23,7 @@ fn main() {
     let (points_b, colors_b) = preprocessing::preprocess_point_cloud(&points_b, &colors_b);
     println!("Performing knn search");
     let knn_indices_a = knn_search::knn_search(&points_a, &points_a, search_size);
-    let knn_indices_b = knn_search::knn_search(&points_a, &points_b, search_size);
+    let knn_indices_b = knn_search::knn_search(&points_b, &points_a, search_size);
     println!("Computing local features");
     let local_features = features::compute_features(
         &points_a,
