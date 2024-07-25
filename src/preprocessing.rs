874631823086c3aa009705b2e4fff31e4c2a4dbf
akaTsunemori <arthur_07q@hotmail.com>
2024-07-25 00:34:58 -0300

    Overall polishing

--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -3,7 +3,7 @@ use na::DMatrix;
 use ordered_float::OrderedFloat;
 use std::collections::BTreeMap;
 
-fn vec_mean(
+fn column_axis_mean(
     vector: &Vec<(OrderedFloat<f64>, OrderedFloat<f64>, OrderedFloat<f64>)>,
 ) -> (f64, f64, f64) {
     let vec_len = vector.len() as f64;
@@ -51,7 +51,7 @@ pub fn duplicate_merging(points: DMatrix<f64>, colors: DMatrix<u8>) -> (DMatrix<
             utils::from_ordered(key.2),
         );
         let colors = points_map.get(&key).unwrap();
-        let colors_mean = vec_mean(colors);
+        let colors_mean = column_axis_mean(colors);
         points_result[(i, 0)] = point.0;
         points_result[(i, 1)] = point.1;
         points_result[(i, 2)] = point.2;
@@ -63,26 +63,18 @@ pub fn duplicate_merging(points: DMatrix<f64>, colors: DMatrix<u8>) -> (DMatrix<
 }
 
 fn rgb_to_yuv(rgb: DMatrix<u8>) -> DMatrix<u8> {
-    let (rows, cols) = rgb.shape();
-    assert!(
-        cols == 3,
-        "Input matrix must have 3 columns representing RGB values"
-    );
+    let rows = rgb.nrows();
     let rgb_f64 = rgb.map(|val| val as f64);
     let r = rgb_f64.column(0);
     let g = rgb_f64.column(1);
     let b = rgb_f64.column(2);
-    let c = DMatrix::from_row_slice(
-        3,
-        3,
-        &[
-            0.2126, 0.7152, 0.0722, -0.1146, -0.3854, 0.5000, 0.5000, -0.4542, -0.0468,
-        ],
-    );
-    let o = na::DVector::from_row_slice(&[0.0, 128.0, 128.0]);
-    let y = (c[(0, 0)] * &r + c[(0, 1)] * &g + c[(0, 2)] * &b).add_scalar(o[0]);
-    let u = (c[(1, 0)] * &r + c[(1, 1)] * &g + c[(1, 2)] * &b).add_scalar(o[1]);
-    let v = (c[(2, 0)] * &r + c[(2, 1)] * &g + c[(2, 2)] * &b).add_scalar(o[2]);
+    let c = [
+        0.2126, 0.7152, 0.0722, -0.1146, -0.3854, 0.5000, 0.5000, -0.4542, -0.0468,
+    ];
+    let o = [0., 128., 128.];
+    let y = (c[0] * &r + c[1] * &g + c[2] * &b).add_scalar(o[0]);
+    let u = (c[3] * &r + c[4] * &g + c[5] * &b).add_scalar(o[1]);
+    let v = (c[6] * &r + c[7] * &g + c[8] * &b).add_scalar(o[2]);
     let mut yuv = DMatrix::zeros(rows, 3);
     for i in 0..rows {
         yuv[(i, 0)] = y[i].round() as u8;
