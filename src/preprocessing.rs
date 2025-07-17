c71d89b95bd6a1fbf1ab1ea2ae0c320c52497302
Arthur Carvalho <arthurhscarvalho@outlook.com>
2025-07-16 21:54:49 -0300

    Compute predictors in parallel, and minor refinements on preprocessing

--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -16,7 +16,21 @@ fn from_ordered_point(num: OrderedPoint) -> [f32; 3] {
     [num.0.into(), num.1.into(), num.2.into()]
 }
 
-fn colors_mean(colors: Vec<[u8; 3]>) -> [u8; 3] {
+fn rgb_to_yuv(rgb: [u8; 3]) -> [u8; 3] {
+    let r = rgb[0] as f32;
+    let g = rgb[1] as f32;
+    let b = rgb[2] as f32;
+    let y = (0.2126 * r + 0.2126 * g + 0.0722 * b) + 0.;
+    let u = (-0.1146 * r + -0.3854 * g + 0.5000 * b) + 128.;
+    let v = (0.5000 * r + -0.4542 * g + -0.0468 * b) + 128.;
+    [
+        y.round().clamp(0.0, 255.0) as u8,
+        u.round().clamp(0.0, 255.0) as u8,
+        v.round().clamp(0.0, 255.0) as u8,
+    ]
+}
+
+fn mean_colors(colors: Vec<[u8; 3]>) -> [u8; 3] {
     let vec_len = colors.len() as f32;
     let mut vec_sum = (0, 0, 0);
     for color in colors {
@@ -37,7 +51,7 @@ fn colors_mean(colors: Vec<[u8; 3]>) -> [u8; 3] {
     rounded_mean
 }
 
-pub fn duplicate_merging(
+pub fn preprocess_point_cloud(
     points: Vec<[f32; 3]>,
     colors: Vec<[u8; 3]>,
 ) -> (Vec<[f32; 3]>, Vec<[u8; 3]>) {
@@ -53,37 +67,11 @@ pub fn duplicate_merging(
     let mut points_result = Vec::with_capacity(nrows);
     let mut colors_result = Vec::with_capacity(nrows);
     for (ordered_point, colors) in points_map {
-        let point = from_ordered_point(ordered_point);
-        let colors_mean = colors_mean(colors);
-        points_result.push(point);
-        colors_result.push(colors_mean);
+        let unique_point = from_ordered_point(ordered_point);
+        let mean_color = mean_colors(colors);
+        let color_yuv = rgb_to_yuv(mean_color);
+        points_result.push(unique_point);
+        colors_result.push(color_yuv);
     }
     (points_result, colors_result)
-}
-
-fn rgb_to_yuv(rgb: Vec<[u8; 3]>) -> Vec<[u8; 3]> {
-    rgb.into_iter()
-        .map(|color| {
-            let r = color[0] as f32;
-            let g = color[1] as f32;
-            let b = color[2] as f32;
-            let y = (0.2126 * r + 0.2126 * g + 0.0722 * b) + 0.;
-            let u = (-0.1146 * r + -0.3854 * g + 0.5000 * b) + 128.;
-            let v = (0.5000 * r + -0.4542 * g + -0.0468 * b) + 128.;
-            [
-                y.round().clamp(0.0, 255.0) as u8,
-                u.round().clamp(0.0, 255.0) as u8,
-                v.round().clamp(0.0, 255.0) as u8,
-            ]
-        })
-        .collect()
-}
-
-pub fn preprocess_point_cloud(
-    points: Vec<[f32; 3]>,
-    colors: Vec<[u8; 3]>,
-) -> (Vec<[f32; 3]>, Vec<[u8; 3]>) {
-    let (points, colors) = duplicate_merging(points, colors);
-    let colors = rgb_to_yuv(colors);
-    (points, colors)
 }
